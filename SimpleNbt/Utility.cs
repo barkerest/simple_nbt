5f0f907008d5a75e163ae4e1b955ed4245671c45
Beau Barker <bbarker@ckindustrial.com>
2019-12-21 22:07:24 -0500

    Add list converters.

--- SimpleNbt/Utility.cs
+++ SimpleNbt/Utility.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.IO.Compression;
+using System.Linq;
 using SimpleNbt.Converters;
 using SimpleNbt.Tags;
 
@@ -11,6 +12,23 @@ namespace SimpleNbt
 	{
 		#region Default Converters
 
+        private static long[] GuidToLongArray(Guid v)
+        {
+            var b = v.ToByteArray();
+            var ret = new long[2];
+            ret[0] = BitConverter.ToInt64(b, 0);
+            ret[1] = BitConverter.ToInt64(b, 8);
+            return ret;
+        }
+
+        private static Guid GuidFromLongArray(long[] v)
+        {
+            var b = new byte[16];
+            BitConverter.GetBytes(v[0]).CopyTo(b, 0);
+            BitConverter.GetBytes(v[1]).CopyTo(b, 8);
+            return new Guid(b);
+        }
+        
         private static readonly INamedBinaryTagConverter[] DefaultConverters =
         {
             new SimpleTypeConverter<StringTag, string>(nm => new StringTag(nm)), 
@@ -38,24 +56,80 @@ namespace SimpleNbt
             new SimpleTypeConverter<DoubleTag, double, decimal>(nm => new DoubleTag(nm), v => (decimal)v, v => (double)v),
             new SimpleTypeConverter<StringTag, string, Guid>(nm => new StringTag(nm), v => Guid.Parse(v), v => v.ToString()), 
             new SimpleTypeConverter<ByteArrayTag, byte[], Guid>(nm => new ByteArrayTag(nm), v => new Guid(v), v => v.ToByteArray()),
-            new SimpleTypeConverter<LongArrayTag, long[], Guid>(nm => new LongArrayTag(nm), 
-                v => {
-                    var b = new byte[16];
-                    BitConverter.GetBytes(v[0]).CopyTo(b, 0);
-                    BitConverter.GetBytes(v[1]).CopyTo(b, 8);
-                    return new Guid(b);
-                },
-                v => {
-                    var b = v.ToByteArray();
-                    var ret = new long[2];
-                    ret[0] = BitConverter.ToInt64(b, 0);
-                    ret[1] = BitConverter.ToInt64(b, 8);
-                    return ret;
-                }
-            ),
+            new SimpleTypeConverter<LongArrayTag, long[], Guid>(nm => new LongArrayTag(nm), GuidFromLongArray, GuidToLongArray),
             new SimpleTypeConverter<ByteArrayTag, byte[]>(nm => new ByteArrayTag(nm)),
             new SimpleTypeConverter<IntArrayTag, int[]>(nm => new IntArrayTag(nm)),
             new SimpleTypeConverter<LongArrayTag, long[]>(nm => new LongArrayTag(nm)), 
+            
+            new SimpleListConverter<ListTag<StringTag>, StringTag, string, string[]>(nm => new ListTag<StringTag>(nm), v => v.ToArray()),
+            new SimpleListConverter<ListTag<StringTag>, StringTag, string, List<string>>(nm => new ListTag<StringTag>(nm), v => v.ToList()),
+            new SimpleListConverter<ListTag<StringTag>, StringTag, string, IEnumerable<string>>(nm => new ListTag<StringTag>(nm), v => v),
+            
+            new SimpleListConverter<ListTag<ByteTag>, ByteTag, sbyte, sbyte[]>(nm => new ListTag<ByteTag>(nm), v => v.ToArray()),
+            new SimpleListConverter<ListTag<ByteTag>, ByteTag, sbyte, byte[], byte>(nm => new ListTag<ByteTag>(nm), v => v.ToArray(), v => unchecked((byte)v), v => unchecked((sbyte)v)),
+            new SimpleListConverter<ListTag<ByteTag>, ByteTag, sbyte, List<sbyte>>(nm => new ListTag<ByteTag>(nm), v => v.ToList()),
+            new SimpleListConverter<ListTag<ByteTag>, ByteTag, sbyte, List<byte>, byte>(nm => new ListTag<ByteTag>(nm), v => v.ToList(), v => unchecked((byte)v), v => unchecked((sbyte)v)),
+            new SimpleListConverter<ListTag<ByteTag>, ByteTag, sbyte, IEnumerable<sbyte>>(nm => new ListTag<ByteTag>(nm), v => v),
+            new SimpleListConverter<ListTag<ByteTag>, ByteTag, sbyte, IEnumerable<byte>, byte>(nm => new ListTag<ByteTag>(nm), v => v, v => unchecked((byte)v), v => unchecked((sbyte)v)),
+
+            new SimpleListConverter<ListTag<IntTag>, IntTag, int, int[]>(nm => new ListTag<IntTag>(nm), v => v.ToArray()),
+            new SimpleListConverter<ListTag<IntTag>, IntTag, int, uint[], uint>(nm => new ListTag<IntTag>(nm), v => v.ToArray(), v => unchecked((uint)v), v => unchecked((int)v)),
+            new SimpleListConverter<ListTag<IntTag>, IntTag, int, List<int>>(nm => new ListTag<IntTag>(nm), v => v.ToList()),
+            new SimpleListConverter<ListTag<IntTag>, IntTag, int, List<uint>, uint>(nm => new ListTag<IntTag>(nm), v => v.ToList(), v => unchecked((uint)v), v => unchecked((int)v)),
+            new SimpleListConverter<ListTag<IntTag>, IntTag, int, IEnumerable<int>>(nm => new ListTag<IntTag>(nm), v => v),
+            new SimpleListConverter<ListTag<IntTag>, IntTag, int, IEnumerable<uint>, uint>(nm => new ListTag<IntTag>(nm), v => v, v => unchecked((uint)v), v => unchecked((int)v)),
+
+            new SimpleListConverter<ListTag<LongTag>, LongTag, long, long[]>(nm => new ListTag<LongTag>(nm), v => v.ToArray()),
+            new SimpleListConverter<ListTag<LongTag>, LongTag, long, ulong[], ulong>(nm => new ListTag<LongTag>(nm), v => v.ToArray(), v => unchecked((ulong)v), v => unchecked((long)v)),
+            new SimpleListConverter<ListTag<LongTag>, LongTag, long, List<long>>(nm => new ListTag<LongTag>(nm), v => v.ToList()),
+            new SimpleListConverter<ListTag<LongTag>, LongTag, long, List<ulong>, ulong>(nm => new ListTag<LongTag>(nm), v => v.ToList(), v => unchecked((ulong)v), v => unchecked((long)v)),
+            new SimpleListConverter<ListTag<LongTag>, LongTag, long, IEnumerable<long>>(nm => new ListTag<LongTag>(nm), v => v),
+            new SimpleListConverter<ListTag<LongTag>, LongTag, long, IEnumerable<ulong>, ulong>(nm => new ListTag<LongTag>(nm), v => v, v => unchecked((ulong)v), v => unchecked((long)v)),
+
+            new SimpleListConverter<ListTag<SingleTag>, SingleTag, float, float[]>(nm => new ListTag<SingleTag>(nm), v => v.ToArray()),
+            new SimpleListConverter<ListTag<SingleTag>, SingleTag, float, List<float>>(nm => new ListTag<SingleTag>(nm), v => v.ToList()),
+            new SimpleListConverter<ListTag<SingleTag>, SingleTag, float, IEnumerable<float>>(nm => new ListTag<SingleTag>(nm), v => v),
+
+            new SimpleListConverter<ListTag<DoubleTag>, DoubleTag, double, double[]>(nm => new ListTag<DoubleTag>(nm), v => v.ToArray()),
+            new SimpleListConverter<ListTag<DoubleTag>, DoubleTag, double, List<double>>(nm => new ListTag<DoubleTag>(nm), v => v.ToList()),
+            new SimpleListConverter<ListTag<DoubleTag>, DoubleTag, double, IEnumerable<double>>(nm => new ListTag<DoubleTag>(nm), v => v),
+
+            new SimpleListConverter<ListTag<DoubleTag>, DoubleTag, double, decimal[], decimal>(nm => new ListTag<DoubleTag>(nm), v => v.ToArray(), v => (decimal)v, v => (double)v),
+            new SimpleListConverter<ListTag<DoubleTag>, DoubleTag, double, List<decimal>, decimal>(nm => new ListTag<DoubleTag>(nm), v => v.ToList(), v => (decimal)v, v => (double)v),
+            new SimpleListConverter<ListTag<DoubleTag>, DoubleTag, double, IEnumerable<decimal>, decimal>(nm => new ListTag<DoubleTag>(nm), v => v, v => (decimal)v, v => (double)v),
+            new SimpleListConverter<ListTag<IntArrayTag>, IntArrayTag, int[], decimal[], decimal>(nm => new ListTag<IntArrayTag>(nm), v => v.ToArray(), v => new decimal(v), v => decimal.GetBits(v)),
+            new SimpleListConverter<ListTag<IntArrayTag>, IntArrayTag, int[], List<decimal>, decimal>(nm => new ListTag<IntArrayTag>(nm), v => v.ToList(), v => new decimal(v), v => decimal.GetBits(v)),
+            new SimpleListConverter<ListTag<IntArrayTag>, IntArrayTag, int[], IEnumerable<decimal>, decimal>(nm => new ListTag<IntArrayTag>(nm), v => v, v => new decimal(v), v => decimal.GetBits(v)),
+
+            new SimpleListConverter<ListTag<ByteArrayTag>, ByteArrayTag, byte[], byte[][]>(nm => new ListTag<ByteArrayTag>(nm), v => v.ToArray()),
+            new SimpleListConverter<ListTag<ByteArrayTag>, ByteArrayTag, byte[], List<byte[]>>(nm => new ListTag<ByteArrayTag>(nm), v => v.ToList()),
+            new SimpleListConverter<ListTag<ByteArrayTag>, ByteArrayTag, byte[], IEnumerable<byte[]>>(nm => new ListTag<ByteArrayTag>(nm), v => v),
+
+            new SimpleListConverter<ListTag<IntArrayTag>, IntArrayTag, int[], int[][]>(nm => new ListTag<IntArrayTag>(nm), v => v.ToArray()),
+            new SimpleListConverter<ListTag<IntArrayTag>, IntArrayTag, int[], List<int[]>>(nm => new ListTag<IntArrayTag>(nm), v => v.ToList()),
+            new SimpleListConverter<ListTag<IntArrayTag>, IntArrayTag, int[], IEnumerable<int[]>>(nm => new ListTag<IntArrayTag>(nm), v => v),
+
+            new SimpleListConverter<ListTag<LongArrayTag>, LongArrayTag, long[], long[][]>(nm => new ListTag<LongArrayTag>(nm), v => v.ToArray()),
+            new SimpleListConverter<ListTag<LongArrayTag>, LongArrayTag, long[], List<long[]>>(nm => new ListTag<LongArrayTag>(nm), v => v.ToList()),
+            new SimpleListConverter<ListTag<LongArrayTag>, LongArrayTag, long[], IEnumerable<long[]>>(nm => new ListTag<LongArrayTag>(nm), v => v),
+
+            new SimpleListConverter<ListTag<ByteTag>, ByteTag, sbyte, bool[], bool>(nm => new ListTag<ByteTag>(nm), v => v.ToArray(), v => (v != 0), v => (sbyte)(v ? 1 : 0)),
+            new SimpleListConverter<ListTag<ByteTag>, ByteTag, sbyte, List<bool>, bool>(nm => new ListTag<ByteTag>(nm), v => v.ToList(), v => (v != 0), v => (sbyte)(v ? 1 : 0)),
+            new SimpleListConverter<ListTag<ByteTag>, ByteTag, sbyte, IEnumerable<bool>, bool>(nm => new ListTag<ByteTag>(nm), v => v, v => (v != 0), v => (sbyte)(v ? 1 : 0)),
+
+            new SimpleListConverter<ListTag<ByteArrayTag>, ByteArrayTag, byte[], Guid[], Guid>(nm => new ListTag<ByteArrayTag>(nm), v => v.ToArray(), v => new Guid(v), v => v.ToByteArray()),
+            new SimpleListConverter<ListTag<ByteArrayTag>, ByteArrayTag, byte[], List<Guid>, Guid>(nm => new ListTag<ByteArrayTag>(nm), v => v.ToList(), v => new Guid(v), v => v.ToByteArray()),
+            new SimpleListConverter<ListTag<ByteArrayTag>, ByteArrayTag, byte[], IEnumerable<Guid>, Guid>(nm => new ListTag<ByteArrayTag>(nm), v => v, v => new Guid(v), v => v.ToByteArray()),
+            new SimpleListConverter<ListTag<LongArrayTag>, LongArrayTag, long[], Guid[], Guid>(nm => new ListTag<LongArrayTag>(nm), v => v.ToArray(), GuidFromLongArray, GuidToLongArray),
+            new SimpleListConverter<ListTag<LongArrayTag>, LongArrayTag, long[], List<Guid>, Guid>(nm => new ListTag<LongArrayTag>(nm), v => v.ToList(), GuidFromLongArray, GuidToLongArray),
+            new SimpleListConverter<ListTag<LongArrayTag>, LongArrayTag, long[], IEnumerable<Guid>, Guid>(nm => new ListTag<LongArrayTag>(nm), v => v, GuidFromLongArray, GuidToLongArray),
+            new SimpleListConverter<ListTag<StringTag>, StringTag, string, Guid[], Guid>(nm => new ListTag<StringTag>(nm), v => v.ToArray(), v => Guid.Parse(v), v => v.ToString()),
+            new SimpleListConverter<ListTag<StringTag>, StringTag, string, List<Guid>, Guid>(nm => new ListTag<StringTag>(nm), v => v.ToList(), v => Guid.Parse(v), v => v.ToString()),
+            new SimpleListConverter<ListTag<StringTag>, StringTag, string, IEnumerable<Guid>, Guid>(nm => new ListTag<StringTag>(nm), v => v, v => Guid.Parse(v), v => v.ToString()),
+
+            new SimpleListConverter<ListTag<LongTag>, LongTag, long, DateTime[], DateTime>(nm => new ListTag<LongTag>(nm), v => v.ToArray(), v => new DateTime(v), v => v.Ticks),
+            new SimpleListConverter<ListTag<LongTag>, LongTag, long, List<DateTime>, DateTime>(nm => new ListTag<LongTag>(nm), v => v.ToList(), v => new DateTime(v), v => v.Ticks),
+            new SimpleListConverter<ListTag<LongTag>, LongTag, long, IEnumerable<DateTime>, DateTime>(nm => new ListTag<LongTag>(nm), v => v, v => new DateTime(v), v => v.Ticks),
             
         };
         
